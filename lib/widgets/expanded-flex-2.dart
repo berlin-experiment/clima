70b9842fc956da97606d3e2305fdb5f75132414a
Stacey Kenny <83240023+berlin-experiment@users.noreply.github.com>
2022-11-29 15:59:53 +0100

    expanded widget at flex 2 extracted

---
+++ lib/widgets/expanded-flex-2.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ExpandedFlexTwo extends StatelessWidget {
+  const ExpandedFlexTwo({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(flex: 1, child: Container());
+  }
+}
